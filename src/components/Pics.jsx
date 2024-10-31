036c39ec863866414fcf60f09022457252e305ae
mania605 <kim.minsun83@gmail.com>
2024-10-31 14:24:49 +0900

    스크롤시 가로 이동을 위한 스타일 설정

--- src/components/Pics.jsx
+++ src/components/Pics.jsx
@@ -1,3 +1,21 @@
-export default function Pics() {
-	return <section className='pics'>Pics</section>;
+export default function Pics({ Scroll, pos }) {
+	return (
+		<section className='pics'>
+			<h2>Gallery Preview</h2>
+			<div className='inner'>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		</section>
+	);
 }
