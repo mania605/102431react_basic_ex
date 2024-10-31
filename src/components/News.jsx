54f521eb032bef5b4b44952ebb011f7c490f929b
mania605 <kim.minsun83@gmail.com>
2024-10-31 11:32:22 +0900

    3d효과 적용 및 2번째 박스 영역 컨텐츠 스타일링

---
+++ src/components/News.jsx
@@ -0,0 +1,11 @@
+export default function News() {
+	return (
+		<section className='news'>
+			<h2>POST PREVIEW</h2>
+			<h3>INFORMATION</h3>
+			<article></article>
+			<article></article>
+			<article></article>
+		</section>
+	);
+}
---
+++ src/components/Visual.jsx
@@ -0,0 +1,17 @@
+export default function Visual({ Scroll }) {
+	const style_h2 = {
+		transform: `translateX(${Scroll}px) translateY(${Scroll}px) rotate(${Scroll * 1.5}deg) scale(${1 + Scroll / 400})`,
+		opacity: 1 - Scroll / 400
+	};
+	const style_box1 = {
+		transform: `rotateY(${Scroll}deg)`,
+		opacity: 1 - Scroll / 800
+	};
+
+	return (
+		<section className='visual'>
+			<h2 style={style_h2}>ALPACO</h2>
+			<div style={style_box1} className='box1'></div>
+		</section>
+	);
+}
