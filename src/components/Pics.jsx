f14936b4882163396bdbea3067aff83ceba4ce59
mania605 <kim.minsun83@gmail.com>
2024-10-31 14:53:07 +0900

    가로 스크롤 이동처리

--- src/components/Pics.jsx
+++ src/components/Pics.jsx
@@ -1,9 +1,25 @@
 export default function Pics({ Scroll, pos }) {
+	const currentScroll = Scroll - pos || 0;
+	// 스크롤값이 현재 섹션에 도달하는 순간 position을 fixed로 현재 세로위치값에 프레임 고정처리
+	const style_inner = {
+		position: Scroll >= pos ? 'fixed' : 'relative',
+		top: 0,
+		left: 0
+	};
+	// 제목도 위와 마찬지로 현재 세로위치값 고정
+	const style_h2 = {
+		position: Scroll >= pos ? 'fixed' : 'absolute',
+		top: 200
+	};
+	//고정된 프레임 안쪽에서 ul을 왼쪽이로 이동 (가로로 스크롤되는 것 같은 착시 효과)
+	const style_ul = {
+		marginLeft: -currentScroll
+	};
 	return (
 		<section className='pics'>
-			<h2>Gallery Preview</h2>
-			<div className='inner'>
-				<ul>
+			<h2 style={style_h2}>Gallery Preview</h2>
+			<div style={style_inner} className='inner'>
+				<ul style={style_ul}>
 					<li></li>
 					<li></li>
 					<li></li>
